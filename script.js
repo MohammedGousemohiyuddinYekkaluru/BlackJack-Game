21dfefacc4692ac990c7d0d51cdcc146ef4426c2
mohammed gouse <gousemohammed163@gmail.com>
2025-11-21 09:52:53 +0530

    Add BlackJack Game

---
+++ script.js
@@ -0,0 +1,29 @@
+const startBtn = document.querySelector('.start-btn');
+const messageText = document.querySelector('.message');
+const sumEl = document.querySelector('.sum-el');
+const cardsEl = document.querySelector('.cards-el');
+
+let firstCard = 10
+let secondCard = 11
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+
+startBtn.addEventListener('click', () => {
+
+    sumEl.textContent = `Sum : ${sum}`
+    cardsEl.textContent = `Cards : ${firstCard} ${secondCard}`
+
+    if(sum <= 20){
+        message = "Do you want to draw a new card?"
+    }else if(sum === 21){
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    }else{
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageText.textContent = message
+})
+
