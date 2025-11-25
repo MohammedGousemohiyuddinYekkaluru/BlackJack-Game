25ff9c55a3b6be0f4624fa7a517b4524e2eda1b3
mohammed gouse <gousemohammed163@gmail.com>
2025-11-25 10:22:09 +0530

    Black jack Game

--- script.js
+++ script.js
@@ -1,65 +1,71 @@
-const startBtn = document.querySelector('.start-btn');
-const messageText = document.querySelector('.message');
-const sumEl = document.querySelector('.sum-el');
-const cardsEl = document.querySelector('.cards-el');
-const newCardBtn = document.querySelector('.newcard-btn')
+const startBtn = document.querySelector(".start-btn");
+const messageText = document.querySelector(".message");
+const sumEl = document.querySelector(".sum-el");
+const cardsEl = document.querySelector(".cards-el");
+const newCardBtn = document.querySelector(".newcard-btn");
+const playerel = document.querySelector("#player-el");
 
-let cards = []
-let sum = 0
-let hasBlackJack = false
-let isAlive = false
-let message = ""
+let cards = [];
+let sum = 0;
+let hasBlackJack = false;
+let isAlive = false;
+let message = "";
+
+let player = {
+    name : "Gouse",
+    chips : 150
+}
+
+playerel.textContent = `${player.name} : $${player.chips}`
 
 function getRandomNumber() {
-    let randomNumber = Math.floor(Math.random() * 13) + 1 //1 -> 13
+  let randomNumber = Math.floor(Math.random() * 13) + 1; //1 -> 13
 
-    if(randomNumber > 10){
-        return 10
-    }else if(randomNumber === 1){
-        return 11
-    }else{
-        return randomNumber
-    }
-    
+  if (randomNumber > 10) {
+    return 10;
+  } else if (randomNumber === 1) {
+    return 11;
+  } else {
+    return randomNumber;
+  }
 }
 
 function startGame() {
-    isAlive = true
-    let firstCard = getRandomNumber()
-    let secondCard = getRandomNumber()
-    sum += firstCard + secondCard
-    cards.push(firstCard, secondCard)
-    renderGame()
+  isAlive = true;
+  let firstCard = getRandomNumber();
+  let secondCard = getRandomNumber();
+  sum += firstCard + secondCard;
+  cards.push(firstCard, secondCard);
+  renderGame();
 }
 
 function renderGame() {
-    sumEl.textContent = `Sum : ${sum}`
-    cardsEl.textContent = "Cards : "
-    for(let i = 0; i <= cards.length - 1; i++) {
-        cardsEl.textContent +=  `${cards[i]} `
-    }
+  sumEl.textContent = `Sum : ${sum}`;
+  cardsEl.textContent = "Cards : ";
+  for (let i = 0; i <= cards.length - 1; i++) {
+    cardsEl.textContent += `${cards[i]} `;
+  }
 
-    if(sum <= 20){
-        message = "Do you want to draw a new card?"
-    }else if(sum === 21){
-        message = "You've got Blackjack!"
-        hasBlackJack = true
-    }else{
-        message = "You're out of the game!"
-        isAlive = false
-    }
-    messageText.textContent = message
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?";
+  } else if (sum === 21) {
+    message = "You've got Blackjack!";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game!";
+    isAlive = false;
+  }
+  messageText.textContent = message;
 }
 
-startBtn.addEventListener('click', startGame)
-
+startBtn.addEventListener("click", startGame);
 
 function newCard() {
-    const newCard = getRandomNumber()
-    sum += newCard
-    cards.push(newCard)
-    renderGame()
+  if (isAlive === true && hasBlackJack === false) {
+    const newCard = getRandomNumber();
+    sum += newCard;
+    cards.push(newCard);
+    renderGame();
+  }
 }
-newCardBtn.addEventListener('click', newCard)
-
-
+newCardBtn.addEventListener("click", newCard);
